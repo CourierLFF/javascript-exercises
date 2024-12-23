8c5995dfdb383ad88374bd8a3bf7b9dffb083921
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2024-12-23 10:28:28 -0500

    finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedItems) {
+    removedArray = array
+    for(let i = 0; i < removedItems.length; i++) {
+        while(removedArray.indexOf(removedItems[i]) != -1) {
+            removedArray.splice(removedArray.indexOf(removedItems[i]), 1);
+        }  
+    }
+    return removedArray;
 };
 
 // Do not edit below this line
