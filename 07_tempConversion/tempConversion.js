d1000c3fca87bc588b64793c90f76b87993b3bec
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2024-12-23 10:59:44 -0500

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let returnedTemp = temp;
+    returnedTemp = (returnedTemp - 32) * (5/9)
+    if(!Number.isInteger(returnedTemp)) {
+      returnedTemp = returnedTemp.toFixed(1);
+    }
+    return Number(returnedTemp)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let returnedTemp = temp;
+    returnedTemp = returnedTemp * (9/5) + 32
+    if(!Number.isInteger(returnedTemp)) {
+      returnedTemp = returnedTemp.toFixed(1);
+    }
+    return Number(returnedTemp)
 };
 
 // Do not edit below this line
