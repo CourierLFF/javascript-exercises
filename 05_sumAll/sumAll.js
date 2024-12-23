69b1221a292b7644cd4ac46f219d21e452d56685
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2024-12-23 10:44:18 -0500

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let result = 0;
+    
+    if(startNum < 0 || endNum < 0) {
+        return "ERROR"
+    }
 
+    if(!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+        return "ERROR"
+    } 
+
+    if(startNum < endNum) {
+        for(let i = startNum; i < endNum + 1; i++) {
+            result += i;
+        }
+    }
+    else {
+        for(let i = endNum; i < startNum + 1; i++) {
+            result += i;
+        }
+    }
+    return result; 
 };
 
 // Do not edit below this line
