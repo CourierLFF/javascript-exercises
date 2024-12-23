e0adbdff12fa25ccd5040cb62bfd5eb813e212ba
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2024-12-23 09:42:28 -0500

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("")
+    let reversedLetters = splitString.reverse()
+    return reversedLetters.join("")
 };
 
 // Do not edit below this line
