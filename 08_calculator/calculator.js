6a1dfa1d6591ccf986b0a8aadee73824cee8f4ef
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-06 15:28:48 -0500

    Finish 08_Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  if(sumArray.length === 0) {
+    return 0;
+  }
+  else {
+    return sumArray.reduce((total, currentItem) => {
+      return total + currentItem;
+    });
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  if(numArray.length === 0) {
+    return 0;
+  }
+  else {
+    return numArray.reduce((total, currentItem) => {
+      return total * currentItem;
+    });
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let finished_product = 1;
+	for(let i = 1; i < num + 1; i++) {
+    finished_product *= i
+  } 
+  return finished_product;
 };
 
 // Do not edit below this line
