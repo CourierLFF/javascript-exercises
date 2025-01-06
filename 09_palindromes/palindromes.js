7ee349961641893d00351b5c987085de35f31e8c
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-06 15:55:25 -0500

    Finish 09_Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let originalString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    originalString = originalString.replace(/\s/g, '');
+    let reversedString = originalString.split('').reverse().join('');
+    reversedString = reversedString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    reversedString = reversedString.replace(/\s/g, '');
+    originalString = originalString.toLowerCase();
+    reversedString = reversedString.toLowerCase();
 
+    console.log("OG: " + originalString);
+    console.log("Rev: " + reversedString);
+
+    if(reversedString == originalString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
