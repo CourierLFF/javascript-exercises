5be6f72f7276008aa304ab7768aa7d7154229e1c
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-07 12:43:23 -0500

    Finish getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    title_array = []
+    for(let i = 0; i < books.length; i++) {
+        title_array.push(books[i].title)
+    }
+    return title_array
 };
 
 // Do not edit below this line
