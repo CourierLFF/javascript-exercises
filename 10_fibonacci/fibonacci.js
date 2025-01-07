0cd73a8a42a12501721e8997490ace9f6d3668fc
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-07 12:29:26 -0500

    Finish 10_Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let current_value = 1;
+    let previous_value = 0;
 
+    if (Number(num) == 0) {
+        return 0;
+    }
+
+    if (Number(num) < 0) {
+        return "OOPS"
+    }
+
+    for(let i = 0; i < Number(num) - 1; i++) {
+        let current_value_held = current_value
+        current_value += previous_value;
+        previous_value = current_value_held;
+    }
+    return current_value;
 };
 
 // Do not edit below this line
