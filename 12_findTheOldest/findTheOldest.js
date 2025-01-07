dd6bf45575411b833206480a39f4de3c8b0c897c
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-07 13:03:38 -0500

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentBiggestPerson;
+    let currentBiggestNumber = 0;
+    for(let i = 0; i < people.length; i++) {
+        if(people[i].yearOfDeath == undefined) {
+            people[i].yearOfDeath = 2025;
+        }
+        if(people[i].yearOfDeath - people[i].yearOfBirth > currentBiggestNumber) {
+            currentBiggestPerson = people[i];
+            currentBiggestNumber = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+    }
+    return currentBiggestPerson;
 };
 
 // Do not edit below this line
